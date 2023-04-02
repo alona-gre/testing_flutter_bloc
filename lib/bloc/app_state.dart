65a0757987857745c417e8a5cd68dff78cf2e5e0
Alena Grechka <alyonagrechka1@gmail.com>
2023-04-02 14:21:57 +0100

    step 6

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,66 +1,28 @@
-import 'package:flutter/foundation.dart' show immutable;
-import 'package:testingbloc_course/models.dart';
+import 'dart:typed_data' show Uint8List;
 
-import 'package:collection/collection.dart';
-import 'package:testingbloc_course/strings.dart';
+import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 class AppState {
   final bool isLoading;
-  final LoginError? loginError;
-  final LoginHandle? loginHandle;
-  final Iterable<Note>? fetchedNotes;
+  final Uint8List? data;
+  final Object? error;
 
   const AppState({
     required this.isLoading,
-    required this.loginError,
-    required this.loginHandle,
-    required this.fetchedNotes,
+    required this.data,
+    required this.error,
   });
 
   const AppState.empty()
       : isLoading = false,
-        loginError = null,
-        loginHandle = null,
-        fetchedNotes = null;
+        data = null,
+        error = null;
 
   @override
   String toString() => {
         'isLoading': isLoading,
-        'loginError': loginError,
-        'loginHandle': loginHandle,
-        'fetchedNotes': fetchedNotes,
+        'hasData': data != null,
+        'error': error,
       }.toString();
-
-  @override
-  bool operator ==(covariant AppState other) {
-    final otherPropertiesAreEqual = isLoading == other.isLoading &&
-        loginError == other.loginError &&
-        loginHandle == other.loginHandle;
-
-    if (fetchedNotes == null && other.fetchedNotes == null) {
-      return otherPropertiesAreEqual;
-      //meaning the fetchedNotes are equal,
-      //we only take otherPropertiesAreEqual into consideration
-      //
-    } else {
-      return otherPropertiesAreEqual &&
-          (fetchedNotes?.isEqualTo(other.fetchedNotes) ?? false);
-    }
-  }
-
-  @override
-  int get hashCode => Object.hash(
-        isLoading,
-        loginError,
-        loginHandle,
-        fetchedNotes,
-      );
-}
-
-extension UnorderedEquality on Object {
-  bool isEqualTo(other) => const DeepCollectionEquality.unordered().equals(
-        this,
-        other,
-      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:testingbloc_course/apis/login_api.dart';
-import 'package:testingbloc_course/apis/notes_api.dart';
-import 'package:testingbloc_course/bloc/actions.dart';
-import 'package:testingbloc_course/bloc/app_bloc.dart';
-import 'package:testingbloc_course/bloc/app_state.dart';
-import 'package:testingbloc_course/dialogs/generic_dialog.dart';
-import 'package:testingbloc_course/dialogs/loading_screen.dart';
-import 'package:testingbloc_course/models.dart';
-import 'package:testingbloc_course/strings.dart';
-import 'package:testingbloc_course/views/iterable_list_view.dart';
-import 'package:testingbloc_course/views/login_view.dart';
+import 'package:flutter/material.dart';
+import 'package:testingbloc_course/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,77 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppBloc(
-        loginApi: LoginApi(),
-        notesApi: NotesApi(),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(homePage),
-        ),
-        body: BlocConsumer<AppBloc, AppState>(
-          listener: ((context, appState) {
-            //loading screen
-            if (appState.isLoading) {
-              LoadingScreen.instance().show(
-                context: context,
-                text: pleaseWait,
-              );
-            } else {
-              LoadingScreen.instance().hide();
-            }
-            //display possible errors
-            final loginError = appState.loginError;
-            if (loginError != null) {
-              showGenericDialog<bool>(
-                context: context,
-                title: loginErrorDialogTitle,
-                content: loginErrorDialogContent,
-                optionsBuilder: () => {ok: true},
-              );
-            }
-            //if we are logged in, but we have no fetched notes - fetch them now
-            if (appState.isLoading == false &&
-                appState.loginError == null &&
-                appState.loginHandle == const LoginHandle.fooBar() &&
-                appState.fetchedNotes == null) {
-              context.read<AppBloc>().add(
-                    const LoadNotesAction(),
-                  );
-            }
-          }),
-          builder: (context, appState) {
-            final notes = appState.fetchedNotes;
-            if (notes == null) {
-              return LoginView(
-                onLoginTapped: ((email, password) {
-                  context.read<AppBloc>().add(
-                        LoginAction(
-                          email: email,
-                          password: password,
-                        ),
-                      );
-                }),
-              );
-            } else {
-              return notes.toListView();
-            }
-          },
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
